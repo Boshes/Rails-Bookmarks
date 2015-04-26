ba1dd674f7f06e4cab3ffa993660459776f333b1
Justen Morgan <justenamorgan@gmail.com>
2015-04-25 17:28:57 -0700

    Adjustments

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,7 +5,12 @@ class Bookmark < ActiveRecord::Base
 
   private
   def realurl
-      self.url = "http://" + self.url + ".com"
+	  if self.url[0..7] == "http://"
+		  self.url
+	  elsif self.url[0..8] == "https://"
+		  self.url
+      else
+        self.url = "http://" + self.url
+      end
+    end
   end
-
-end
